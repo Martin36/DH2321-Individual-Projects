4fd97d96dcf497304fc740e817869b75e21955ed
Martin <martin.funkquist@gmail.com>
2017-02-18 15:16:27 +0100

    Added the data to an array.

--- Visualizing the World Through its Own Worldview/JavaScripts/GroupedBubbleChart.js
+++ Visualizing the World Through its Own Worldview/JavaScripts/GroupedBubbleChart.js
@@ -1,6 +1,14 @@
 ﻿//Arrays to hold selections
 var selectedCountries = [];
 var selectedVariables = [];
+//Array containing the data
+var dataArray = [];
+//Array containing the names of the variables
+var variablesArray = [
+  "Feeling of happiness",
+  "Important in life: Family",
+  "Satisfaction with your life"
+];
 
 createCountryBubbles();
 
@@ -8,6 +16,7 @@ loadData();
 
 createListOfVariables();
 
+//createBarChart();
 //Function for creating the grouped bubbles chart with the countries
 function createCountryBubbles() {
 
@@ -50,17 +59,17 @@ function createCountryBubbles() {
        .style("fill", "rgb(158, 154, 200)")
        .on("click", function (d) {
          //Find index of the element
-         var index = selected.indexOf(d.data);
+         var index = selectedCountries.indexOf(d.data);
          //Check if country aleady is selected, otherwise we want to remove it
          if (index >= 0) {
            //Remove the element from the array
-           selected.splice(index, 1);
+           selectedCountries.splice(index, 1);
            //Set color back to normal
            d3.select(this).style("fill", "rgb(158, 154, 200)");
          }
          else {
            //Add the selected country to an array
-           selected.push(d.data);
+           selectedCountries.push(d.data);
            //Set color to red
            d3.select(this).style("fill", "rgb(255, 0, 0)");
          }
@@ -89,13 +98,6 @@ function createCountryBubbles() {
 
 //Function for creating a list with checkboxes of variables where the user can select which ones they want to see
 function createListOfVariables() {
-
-  //Array containing the names of the variables
-  var variablesArray = [
-    "Feeling of happiness",
-    "Important in life",
-    "Satisfaction with your life"
-  ];
 
   //Create an checkbox element for each variable
   for (var i = 0; i < variablesArray.length; i++) {
@@ -127,10 +129,16 @@ function createListOfVariables() {
 
     //Add a break
     div.append("br");
-
-    //Add a button for creating the bar chart
-
   };
+  //Add a button for creating the bar chart
+  d3.select("#createBarchartButton").append("input")
+    .attr("name", "barchartButton")
+    .attr("value", "Create Bar Chart")
+    .attr("type", "button")
+    .on("click", function () {
+      createBarChart();
+    })
+
 }
 
 //Function for loading the data of the variables
@@ -153,8 +161,16 @@ function loadData() {
       for (var i = 0; i < satisfaction.length; i++) {
         clean(satisfaction[i]);
       }
+
+      dataArray[variablesArray[0]] = feelings;
+      dataArray[variablesArray[1]] = family;
+      dataArray[variablesArray[2]] = satisfaction;
+      console.log(dataArray);
+
+      
     })
 }
+
 //http://stackoverflow.com/questions/286141/remove-blank-attributes-from-an-object-in-javascript
 //Remove any properties that are empty i.e. ""
 function clean(obj) {
@@ -164,7 +180,89 @@ function clean(obj) {
     }
   }
 }
+
+//https://bl.ocks.org/mbostock/3886394
 //Function for creating the stacked bar chart with the previously specified variables and countries
 function createBarChart() {
+
+  var svg = d3.select("svg#stackedBarChart"),
+      margin = { top: 20, right: 60, bottom: 30, left: 40 },
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom,
+      g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var x = d3.scaleBand()
+      .rangeRound([0, width])
+      .padding(0.1)
+      .align(0.1);
+
+  var y = d3.scaleLinear()
+      .rangeRound([height, 0]);
+
+  var z = d3.scaleOrdinal()
+      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+  var stack = d3.stack()
+      .offset(d3.stackOffsetExpand);
+
+  d3.csv("Data/Feeling_of_happiness2.csv", type, function (error, data) {
+    if (error) throw error;
+
+    //Summarize the last 3 columns to one column
+    var dataStats = new dataStatistics();
+    data = dataStats.addColumns(data, ['Inappropriate response', 'No answer', "Don´t know"]);
+
+    //console.log(data);
+
+    //data.sort(function (a, b) { return b[data.columns[1]] / b.total - a[data.columns[1]] / a.total; });
+
+    x.domain(data.map(function (d) { return d.Country; }));
+    z.domain(data.columns.slice(1));
+
+    var serie = g.selectAll(".serie")
+      .data(stack.keys(data.columns.slice(1))(data))
+      .enter().append("g")
+        .attr("class", "serie")
+        .attr("fill", function (d) { return z(d.key); });
+
+    serie.selectAll("rect")
+      .data(function (d) { return d; })
+      .enter().append("rect")
+        .attr("x", function (d) { return x(d.data.Country); })
+        .attr("y", function (d) { return y(d[1]); })
+        .attr("height", function (d) { return y(d[0]) - y(d[1]); })
+        .attr("width", x.bandwidth());
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10, "%"));
+
+    var legend = serie.append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d) { var d = d[d.length - 1]; return "translate(" + (x(d.data.Country) + x.bandwidth()) + "," + ((y(d[0]) + y(d[1])) / 2) + ")"; });
+
+    legend.append("line")
+        .attr("x1", -6)
+        .attr("x2", 6)
+        .attr("stroke", "#000");
+
+    legend.append("text")
+        .attr("x", 9)
+        .attr("dy", "0.35em")
+        .attr("fill", "#000")
+        .style("font", "10px sans-serif")
+        .text(function (d) { return d.key; });
+  });
+
+  function type(d, i, columns) {
+    for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+    d.total = t;
+    return d;
+  }
 
 }
